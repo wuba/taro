6f49fc40e3ea2b70d41c8dbde5d13c2ee88f3baa
iChengbo <chengbo_x@163.com>
2021-07-28 17:59:24 +0800

    Revert "feat: 接入 sentry"
    This reverts commit 952b329884f72689b81e917d3e96f48f0dce0b8f.

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,13 @@
  * @Author: iChengbo
  * @Date: 2021-07-19 14:37:08
  * @LastEditors: iChengbo
- * @LastEditTime: 2021-07-28 17:34:38
+ * @LastEditTime: 2021-07-21 14:57:21
  * @FilePath: /taro-react-native/src/app.ts
  */
 import { Component } from 'react'
 import SplashScreen from 'react-native-splash-screen'
-import * as Sentry from "@sentry/react-native";
 
 import './app.scss'
-
-Sentry.init({
-  dsn: "https://bfb6d9d53e4c4374a192fca5a605f45a@o738924.ingest.sentry.io/5785609",
-});
 
 class App extends Component {
 
