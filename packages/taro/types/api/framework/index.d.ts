454eb8556b26fe914ab76fdd7717c47d945c83fe
shinken008 <shinken008@gmail.com>
2021-08-24 15:25:36 +0800

    chore: 所以文件增加 license 头部描述

--- packages/taro/types/api/framework/index.d.ts
+++ packages/taro/types/api/framework/index.d.ts
@@ -1,3 +1,22 @@
+/*
+* Licensed to the O2Team under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 declare namespace Taro {
   /** 注册小程序中的一个页面。接受一个 `Object` 类型参数，其指定页面的初始数据、生命周期回调、事件处理函数等。 */
   interface Page {
